4657ebad2bf041826ceb9452526a43ad635e9aea
Orange baby <81520258+orangebabyb@users.noreply.github.com>
2025-11-24 21:11:07 +0800

    Update foliage_vert.glsl

--- HW3/shaders/foliage_vert.glsl
+++ HW3/shaders/foliage_vert.glsl
@@ -19,7 +19,7 @@ out vec2 v_UV;
 out float v_Layer;
 out vec3 v_Normal;   // [新增] 傳遞法線
 out vec3 v_WorldPos; // [新增] 傳遞世界座標
-
+out vec3 f_viewVertex;
 void main() {
     uint idx = gl_BaseInstance + gl_InstanceID;
     vec4 data = plants[idx].positionAndType;
@@ -30,6 +30,7 @@ void main() {
     v_Layer = data.w;
     v_Normal = a_Normal; // 假設植物不旋轉，直接用法線
     v_WorldPos = worldPos;
-    
+    f_viewVertex = (u_View * vec4(worldPos, 1.0)).xyz;
+
     gl_Position = u_Proj * u_View * vec4(worldPos, 1.0);
 }
